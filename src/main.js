f33305430439db6ff96b63023f1413fb366fab25
Kosdevua <konstantin.vodolozhko@gmail.com>
2024-04-14 12:01:08 +0300

    create markup

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// У файлі main.js напиши всю логіку роботи додатка.
+
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
